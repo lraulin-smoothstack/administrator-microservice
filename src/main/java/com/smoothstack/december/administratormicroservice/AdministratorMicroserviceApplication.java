4f18aabe4d49e072c8196b39f43b23a36a371c07
Lee Raulin <leeraulin@gmail.com>
2020-01-25 15:21:35 -0500

    first commit

---
+++ src/main/java/com/smoothstack/december/administratormicroservice/AdministratorMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.smoothstack.december.administratormicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministratorMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministratorMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smoothstack/december/administratormicroservice/AdministratorMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smoothstack.december.administratormicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministratorMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
